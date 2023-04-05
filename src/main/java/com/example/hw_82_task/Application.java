2ad686f2c4e018cb39b06dde98d86f490ac469b5
Alex <alexeym75@gmail.com>
2023-04-05 18:52:19 +0700

    Initial commit

---
+++ src/main/java/com/example/hw_82_task/Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_82_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
